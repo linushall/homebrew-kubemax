42de238c0f055b8a34ca067ff61f943ee30348c3
Linus Hall <ask.linushall@gmail.com>
2025-03-26 19:42:51 +0100

    add dependencies

--- Formula/kubemax.rb
+++ Formula/kubemax.rb
@@ -5,7 +5,27 @@ class Kubemax < Formula
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   version "1.0"
 
+  depends_on "tmux"
+  depends_on "kubectl"
+
   def install
     bin.install "kubemax.sh" => "kubemax"
   end
+
+  def caveats
+  <<~EOS
+    🛠️  Thank you for installing Kubemax!
+
+    📺 Check out my YouTube channel (@LinusHall) for more tutorials and updates:
+    https://www.youtube.com/channel/UClwhP90fuoKeCExDxzzBCXw
+
+    💡 About the Developer:
+    I am a passionate DevOps engineer who loves building tools and sharing knowledge.
+    Follow me on GitHub for more projects:
+    https://github.com/linushall/kubemax
+
+    🎉 Have a nice day! Linus Hall
+  EOS
+end
+
 end
